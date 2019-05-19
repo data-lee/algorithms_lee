11f0fd5c54955b2bc3dcc14f8fcfec65d17853f3
lee <tb65779454@163.com>
2019-05-19 18:40:18 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	setup.py
    #	test1.txt.txt

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ def long_description():
 
 
 setup(
-    name='algorithms',
+    name='algorithms_lee',
     version='0.1.0',
     description='Pythonic Data Structures and Algorithms',
     long_description=long_description(),
     long_description_content_type="text/markdown",
-    url='https://github.com/keon/algorithms',
-    author='Algorithms Team & Contributors',
-    author_email="kwk236@gmail.com",
+    url='https://github.com/liyang101010/algorithms_lee',
+    author='Algorithms lee',
+    author_email="ly61220271@gmail.com",
     license='MIT',
     packages=find_packages(),
     classifiers=[
