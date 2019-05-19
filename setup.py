e6d28cf84d8b5d161ec23fb997de93963e8a9ad4
lee <tb65779454@163.com>
2019-05-19 18:37:54 +0800

    commit setup.py

--- setup.py
+++ setup.py
@@ -10,20 +10,22 @@ def long_description():
     return readme
 
 
-setup(name='algorithms',
-      version='0.1.0',
-      description='Pythonic Data Structures and Algorithms',
-      long_description=long_description(),
-      long_description_content_type="text/markdown",
-      url='https://github.com/keon/algorithms',
-      author='Algorithms Team & Contributors',
-      author_email="kwk236@gmail.com",
-      license='MIT',
-      packages=find_packages(),
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          ],
-      zip_safe=False)
+setup(
+    name='algorithms',
+    version='0.1.0',
+    description='Pythonic Data Structures and Algorithms',
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/keon/algorithms',
+    author='Algorithms Team & Contributors',
+    author_email="kwk236@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    zip_safe=False)
